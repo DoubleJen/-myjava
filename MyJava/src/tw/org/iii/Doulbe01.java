3c432b6de0c03ec394622de1c8219df25f0a2960
Mac <Mac@Mac-PC>
2017-03-05 13:36:53 +0800

    2017030502

--- MyJava/src/tw/org/iii/Doulbe01.java
+++ MyJava/src/tw/org/iii/Doulbe01.java
@@ -3,6 +3,7 @@ package tw.org.iii;
 public class Doulbe01 {
 
 	public static void main(String[] args) {
+		System.out.println("Hello,Doulbe2");
 		// TODO Auto-generated method stub
 		// TODO ³æ¦Cµù¸Ñ
 	}
