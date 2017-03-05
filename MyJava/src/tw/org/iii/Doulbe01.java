ba878b4cf012e104750a2aaba7ce3e25544e303e
Mac <Mac@Mac-PC>
2017-03-05 13:19:54 +0800

    2017030501

---
+++ MyJava/src/tw/org/iii/Doulbe01.java
@@ -0,0 +1,25 @@
+package tw.org.iii;
+
+public class Doulbe01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO 單列註解
+	}
+		void m1(){
+			/*
+			 * 區塊註解
+			 */
+
+		}
+
+		void m2(){
+		/**
+		 *   @專門給DOC註解
+		 */
+		
+	}
+	
+	
+
+}
